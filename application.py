5c769a3f2cb8525d35344904f9f4750622986169
v_hryhorchuk <v.hryhorchuk@mobidev.biz>
2018-08-08 19:34:20 +0300

    Added pages for skillup

--- application.py
+++ application.py
@@ -9,6 +9,9 @@ from passlib.context import CryptContext
 
 from helpers import *
 
+
+ERROR = "I feel disturbance in force {}"
+
 # configure application
 app = Flask(__name__)
 
@@ -43,206 +46,103 @@ db = SQL("sqlite:///finance.db")
 def index():
     return render_template("index.html")
 
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    if request.method == "POST":
-        msg_error = "Please, provide input"
-
-        if not request.form.get("buy_stock") or not request.form.get("amount"):
-            return render_template("buy.html", error = msg_error)
-
-        #save input from form
-        stock_symb = request.form.get("buy_stock")
-        st_amount = request.form.get("amount")
-
-        #check if amount is digit
-        if not st_amount.isdigit():
-            return render_template("buy.html", error = "amount should be positive integer")
-
-        #check if user has enough cash
-        #1 - get actual cash
-        user_id = session.get("user_id")
-        cash = db.execute("SELECT cash FROM users WHERE id=:id", id = user_id)
-        #2 - check stock price
-        stock_retrieved = lookup(stock_symb)
-
-        if stock_retrieved is None:
-            return apology("Counld not retrieve stock prices")
-
-        stock_price = stock_retrieved["price"]
-
-        sum = float(stock_price) * float(st_amount)
-        #3 - compare cash and stock price
-        cash_float = float(cash[0]["cash"])
-        if cash_float < sum:
-            return render_template("buy.html", error = "You have not enough money")
-
-        #store a bought stocks in table "Archieve"
-        #insert new or update if exist
-        rows = db.execute("SELECT * FROM Archieve WHERE user_id=:user_id AND symbol=:symbol", symbol=stock_retrieved["symbol"], user_id=user_id)
-        #if not exist then insert
-        if len(rows) < 1:
-            rows = db.execute("INSERT INTO Archieve (user_id, symbol, name, shares, price, total) \
-                VALUES (:user_id, :symbol, :name, :shares, :price, :total)",
-                    user_id = user_id, symbol = stock_retrieved["symbol"], name = stock_retrieved["name"],
-                        shares = st_amount, price = stock_retrieved["price"], total = sum)
-        #update if already exist
-        else:
-            rows = db.execute("UPDATE Archieve SET shares=shares+:shares, total=total+:total \
-                WHERE user_id=:user_id AND symbol=:symbol",
-                    user_id = user_id, symbol = stock_retrieved["symbol"], shares = st_amount, total = sum)
-
-        if rows is None:
-            return apology("Server's error")
-
-        #update a cash in the users table
-        result = db.execute("UPDATE users SET cash = :cash WHERE id = :id", cash = cash_float - sum, id = user_id)
-        if result is None:
-            return apology("Server's error")
-
-        # redirect user to home page
-        return redirect(url_for("index"))
-
-    else:
-        return render_template("buy.html")
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions."""
-    return apology("TODO")
-
+@basic_auth.required
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in."""
-
     # forget any user_id
     session.clear()
-
-    # if user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username")
+            return render_template("login.html", error=ERROR.format("Empty username"))
         # ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password")
-
+            return render_template("login.html", error=ERROR.format("Empty password"))
         # query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
-
         # ensure username exists and password is correct
         if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-            return apology("invalid username and/or password")
-
+            return render_template("login.html", error=ERROR.format("Not valid credentials"))
         # remember which user has logged in
         session["user_id"] = rows[0]["id"]
-
         # redirect user to home page
-        return redirect(url_for("index"))
-
-    # else if user reached route via GET (as by clicking a link or via redirect)
+        return redirect(url_for("step2"))
     else:
         return render_template("login.html")
 
+@basic_auth.required
 @app.route("/logout")
 def logout():
-    """Log user out."""
-
     # forget any user_id
     session.clear()
-
     # redirect user to login form
     return redirect(url_for("login"))
 
-@app.route("/quote", methods=["GET", "POST"])
+@basic_auth.required
+@app.route("/step2", methods=["GET", "POST"])
 @login_required
-def quote():
-
-    #for POST display result
+def step2():
     if request.method == "POST":
-        #check if input not empty
-        if not request.form.get("quote"):
-            return apology("must provide a symbol")
-        #save quote from from
-        quote = request.form.get("quote")
-
-        #use helper.lookup to retrieve stock
-        stock_retrieved = lookup(quote)
-        #check if stock is not None
-        if stock_retrieved is None:
-            return apology("Stock not found")
-        #if success render a template with result
-        return render_template("quote_display.html", name = stock_retrieved["name"], price = stock_retrieved["price"], symbol = stock_retrieved["symbol"])
-
-    #for GET display form
+        return render_template("step2.html")
     else:
-        return render_template("quote.html")
+        return render_template("step2.html")
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-
-    session.clear()
-
-    """Register user."""
-    # if user reached route via POST
+@basic_auth.required
+@app.route("/step3", methods=["GET", "POST"])
+@login_required
+def step3():
     if request.method == "POST":
+        return render_template("step3.html")
+    else:
+        return render_template("step3.html")
 
-        # ensure username was submitted
+@basic_auth.required
+@app.route("/step4", methods=["GET", "POST"])
+@login_required
+def step4():
+    if request.method == "POST":
+        return render_template("step4.html")
+    else:
+        return render_template("step4.html")
+
+@basic_auth.required
+@app.route("/step5", methods=["GET", "POST"])
+@login_required
+def step5():
+    if request.method == "POST":
+        return render_template("step5.html")
+    else:
+        return render_template("step5.html")
+
+@basic_auth.required
+@app.route("/dartlord", methods=["GET", "POST"])
+def dartlord():
+    session.clear()
+    if request.method == "POST":
         if not request.form.get("username"):
-            return apology("must provide username")
-
-        # ensure password was submitted
+            return render_template("dartlord.html", error=ERROR.format("Username is empty"))
         elif not request.form.get("password"):
-            return apology("must provide password")
-
+            return render_template("dartlord.html", error=ERROR.format("Password is empty"))
         elif not request.form.get("confirm_pass"):
-            print("not confirmed pass")
-            return apology("must confirm password")
+            return render_template("dartlord.html", error=ERROR.format("Confirm is empty"))
 
-        login = request.form.get("username")
+        log_in = request.form.get("username")
         password = request.form.get("password")
         confirm_pass = request.form.get("confirm_pass")
-
         if password != confirm_pass:
-            return apology("password doesn't match confirm password")
-
+            return render_template("dartlord.html", error=ERROR.format("password doesn't match confirm password"))
         #hashing pass
         scheme_for_hash = CryptContext(schemes=["sha256_crypt"], sha256_crypt__default_rounds=77000)
         hash_of_pass = scheme_for_hash.hash(password)
-
         # insert into database new username
-        rows = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=login, hash=hash_of_pass)
+        rows = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=log_in, hash=hash_of_pass)
         if rows is None:
-            return apology("such user is already registered", 409)
+            return apology("Such user is already registered", 409)
 
         return redirect(url_for("index"))
-
     else:
-        return render_template("register.html")
+        return render_template("dartlord.html")
 
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    if request.method == "POST":
-
-        #sell stock
-        #1 - get stock_total
-        symbol = request.form.get("symbol")
-        user_id = session["user_id"]
-        price_total = db.execute("SELECT total from user WHERE symbol=:symbol AND user_id=:user_id", symbol=symbol, user_id=user_id)
-
-        #2 - updated users - add price to cash
-
-        return redirect(url_for("index"))
-
-    else:
-        user_id = session["user_id"]
-        symbols = db.execute("SELECT symbol FROM Archieve WHERE user_id=:user_id", user_id=user_id)
-        return render_template("sell.html", symbols=symbols)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
